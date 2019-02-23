5e7396e0d91bc88bc29d84645378ea5c1601331f
João José Maranhão Junior <jjjunior@gmail.com>
2019-02-23 00:46:58 -0300

    Inclusão de Controller do Consumo do Servico de pedidos.

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/PedidoServicoController.java
@@ -0,0 +1,33 @@
+package br.com.casadocodigo.loja.controllers;
+
+import br.com.casadocodigo.loja.models.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/pedidos")
+public class PedidoServicoController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView consultaPedido() {
+
+        final String uri = "https://book-payment.herokuapp.com/orders";
+
+        List<Pedido> pedidos = restTemplate.getForObject(uri, List.class);
+
+        ModelAndView modelAndView = new ModelAndView("pedidos");
+
+        modelAndView.addObject("pedidos", pedidos);
+
+        return modelAndView;
+    }
+}
